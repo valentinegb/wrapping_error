dea4074e5bc46b2072a1db935a70bf2e1d1a0fb7
Valentine Briese <valentinegb@icloud.com>
2023-07-20 13:53:36 -0700

    Require wrapped error, add documentation
    Hopefully requiring a wrapped error is not always a feature, see TODO comment.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,38 @@
+//! An anti-boilerplate crate for errors that wrap errors.
+//!
+//! This crate only exports one item: the [`wrapping_error`] macro. See that for documentation.
+
+/// Creates an error that wraps other errors.
+///
+/// ## Example
+///
+/// ```
+/// use std::{env::VarError, io};
+///
+/// use wrapping_error::wrapping_error;
+///
+/// wrapping_error!(pub(crate) AppDataError {
+///     Var(VarError) => "could not get $HOME environment variable",
+///     Io(io::Error) => "failed to read/write app data",
+///     Postcard(postcard::Error) => "failed to serialize/deserialize app data",
+/// });
+/// ```
 #[macro_export]
 macro_rules! wrapping_error {
-    ($vis:vis $enum:ident { $($variant:ident$(($error:path))? => $message:literal),+$(,)? }) => {
+    ($vis:vis $enum:ident { $($variant:ident($error:path)$( => $message:literal)?),+$(,)? }) => {
         use std::{fmt, error};
 
         #[derive(Debug)]
         $vis enum $enum {
-            $($variant$(($error))?,)+
+            $($variant($error),)+
         }
 
         impl fmt::Display for $enum {
             fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
                 match *self {
-                    // TODO: this errors because $error MUST be used in the $((..))? repitition or
-                    // else the compiler doesn't know which repitition this is supposed to be
-                    $(Self::$variant$((..))? => write!(f, $message),)+
+                    // TODO: find a way to make $error optional and be able to tie (..) to whether it exists
+                    $($(Self::$variant(..) => write!(f, $message),)?)+
+                    ref err => err.fmt(f),
                 }
             }
         }
@@ -26,12 +45,12 @@ macro_rules! wrapping_error {
             }
         }
 
-        $($(
+        $(
             impl From<$error> for $enum {
                 fn from(err: $error) -> Self {
                     Self::$variant(err)
                 }
             }
-        )?)+
+        )+
     };
 }
